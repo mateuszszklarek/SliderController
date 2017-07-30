26b8f63fc26445a9865583a5ddbe372e43e0a89d
Mateusz Szklarek <mateusz.szklarek@gmail.com>
2017-07-30 20:48:58 +0200

    Add basic implementation for SliderController

--- Source/SliderController.swift
+++ Source/SliderController.swift
@@ -1,1 +1,25 @@
-final public class SliderController: UIViewController {}
+final public class SliderController: UIViewController {
+
+    override public func loadView() {
+        view = UIView(frame: .zero)
+        addSubviews()
+        setUpLayout()
+    }
+
+    // MARK: Subviews
+
+    private let slider: Slider = Slider(frame: .zero)
+
+    private func addSubviews() {
+        view.addSubview(slider)
+    }
+
+    private func setUpLayout() {
+        slider.translatesAutoresizingMaskIntoConstraints = false
+        slider.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        slider.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        slider.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        slider.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+    }
+    
+}
