d3733134f4c3c962ba567e6fb356079c04b83cf1
Mateusz Szklarek <mateusz.szklarek@gmail.com>
2017-07-30 20:53:11 +0200

    Add basic example usage of SliderController

--- Example/UI/ViewController.swift
+++ Example/UI/ViewController.swift
@@ -1,10 +1,29 @@
 import UIKit
+import SliderController
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    override func loadView() {
+        view = UIView(frame: .zero)
         view.backgroundColor = .white
+        embed(child: sliderController, in: view)
+    }
+
+    let sliderController = SliderController()
+
+}
+
+extension UIViewController {
+
+    func embed(child: UIViewController, in targetView: UIView) {
+        addChildViewController(child)
+        targetView.addSubview(child.view)
+        child.view.translatesAutoresizingMaskIntoConstraints = false
+        child.view.topAnchor.constraint(equalTo: targetView.topAnchor).isActive = true
+        child.view.leftAnchor.constraint(equalTo: targetView.leftAnchor).isActive = true
+        child.view.rightAnchor.constraint(equalTo: targetView.rightAnchor).isActive = true
+        child.view.bottomAnchor.constraint(equalTo: targetView.bottomAnchor).isActive = true
+        child.didMove(toParentViewController: self)
     }
 
 }
