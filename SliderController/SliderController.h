e17cd62c12145fdeb35648907888f08b25b73e0a
Mateusz Szklarek <mateusz.szklarek@gmail.com>
2017-07-30 19:46:32 +0200

    Create SliderController framework

---
+++ SliderController/SliderController.h
@@ -0,0 +1,19 @@
+//
+//  SliderController.h
+//  SliderController
+//
+//  Created by Mateusz Szklarek on 30/07/2017.
+//  Copyright © 2017 Mateusz Szklarek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SliderController.
+FOUNDATION_EXPORT double SliderControllerVersionNumber;
+
+//! Project version string for SliderController.
+FOUNDATION_EXPORT const unsigned char SliderControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SliderController/PublicHeader.h>
+
+
