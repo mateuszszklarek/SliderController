d2984e222bb64acba19b48ca8ab99cb019cb64c1
Mateusz Szklarek <mateusz.szklarek@gmail.com>
2017-07-30 20:05:26 +0200

    Clean up project add example test

--- Source/SliderController.h
+++ Source/SliderController.h
@@ -1,11 +1,3 @@
-//
-//  SliderController.h
-//  SliderController
-//
-//  Created by Mateusz Szklarek on 30/07/2017.
-//  Copyright © 2017 Mateusz Szklarek. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 
 //! Project version number for SliderController.
@@ -13,7 +5,3 @@ FOUNDATION_EXPORT double SliderControllerVersionNumber;
 
 //! Project version string for SliderController.
 FOUNDATION_EXPORT const unsigned char SliderControllerVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <SliderController/PublicHeader.h>
-
-
