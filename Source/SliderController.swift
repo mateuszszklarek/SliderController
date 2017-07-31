e4943d71b36a1087e5c59c2767a1c82d0ec605df
Mateusz Szklarek <mateusz.szklarek@gmail.com>
2017-07-31 23:39:56 +0200

    Set up slider interactions

--- Source/SliderController.swift
+++ Source/SliderController.swift
@@ -15,6 +15,48 @@ final public class SliderController: UIViewController {
         setUpLayout()
     }
 
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setUpSliderInteractions()
+    }
+
+    // MARK: - Interactions
+
+    private func setUpSliderInteractions() {
+        let tapRecognizer = UITapGestureRecognizer(target: self, action: #selector(didTapSlider(gestureRecognizer:)))
+        slider.addGestureRecognizer(tapRecognizer)
+        slider.addTarget(self, action: #selector(sliderValueDidChange(sender:)), for: .valueChanged)
+        let panRecognizer = UIPanGestureRecognizer(target: self, action: #selector(sliderDidSwipe(sender:)))
+        panRecognizer.cancelsTouchesInView = false
+        slider.addGestureRecognizer(panRecognizer)
+    }
+
+    @objc
+    private func didTapSlider(gestureRecognizer: UITapGestureRecognizer) {
+        let tappedPoint = gestureRecognizer.location(in: slider)
+
+        let unitWidth = slider.maximumValue / Float(slider.frame.size.width)
+        let targetValue = Float(tappedPoint.x - slider.frame.origin.x) * unitWidth
+
+        slider.setValue(targetValue, animated: true)
+        delegate?.sliderDidTap(atValue: targetValue)
+    }
+
+    @objc
+    private func sliderValueDidChange(sender: UISlider) {
+        delegate?.sliderValueDidChange(value: sender.value)
+    }
+
+    @objc
+    private func sliderDidSwipe(sender: UIPanGestureRecognizer) {
+        if sender.state == .began {
+            delegate?.sliderDidStartSwiping()
+        } else if sender.state == .ended {
+            delegate?.sliderDidEndSwiping()
+        }
+    }
+
     // MARK: Subviews
 
     private(set) lazy var slider: Slider = Slider(frame: .zero)
