591d6a9d487820084e427a821e09e67a038a546e
Mateusz Szklarek <mateusz.szklarek@gmail.com>
2017-08-01 06:44:24 +0200

    Rename function in SliderControlling protocol

--- Source/SliderController.swift
+++ Source/SliderController.swift
@@ -6,7 +6,7 @@ public protocol SliderControllerDelegate: class {
 }
 
 public protocol SliderControlling: class {
-    func setSliderValue(_ value: Float)
+    func setSlider(value: Float)
     var sliderValue: Float { get }
     var trackHeight: CGFloat { get set }
     var selectedTrackColor: UIColor { get set }
