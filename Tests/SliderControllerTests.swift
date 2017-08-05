d9d2cc4cabab660e0c726afda31e79311dec8cec
Mateusz Szklarek <mateusz.szklarek@gmail.com>
2017-08-05 16:04:31 +0200

    Add two tests for moving slider

--- Tests/SliderControllerTests.swift
+++ Tests/SliderControllerTests.swift
@@ -1,10 +1,49 @@
-import XCTest
 @testable import SliderController
+import XCTest
+import EarlGrey
 
 class SliderControllerTests: XCTestCase {
 
-    func testExample() {
-        XCTAssertEqual(1 + 1, 2)
+    var sut: SliderController!
+    var delegateSpy: SliderControllerDelegateSpy!
+    var application: UIApplication!
+
+    override func setUp() {
+        super.setUp()
+
+        delegateSpy = SliderControllerDelegateSpy()
+        sut = SliderController()
+        sut.unselectedTrackColor = .blue
+        sut.selectedTrackColor = .red
+        sut.delegate = delegateSpy
+
+        application = UIApplication.shared
+        application.keyWindow?.backgroundColor = .white
+        application.keyWindow?.rootViewController = sut
     }
-    
+
+    override func tearDown() {
+        super.tearDown()
+
+        sut = nil
+        delegateSpy = nil
+        application = nil
+    }
+
+    func testMoveSliderToValue0_5() {
+        let targetValue: Float = 0.5
+
+        EarlGrey.select(elementWithMatcher: grey_kindOfClass(UISlider.self))
+            .perform(grey_moveSliderToValue(targetValue))
+            .assert(grey_sliderValueMatcher(grey_equalTo(targetValue)))
+    }
+
+    func testMoveSliderToValue1_0() {
+        let targetValue: Float = 1.0
+
+        EarlGrey.select(elementWithMatcher: grey_kindOfClass(UISlider.self))
+            .perform(grey_moveSliderToValue(targetValue))
+            .assert(grey_sliderValueMatcher(grey_equalTo(targetValue)))
+    }
+
 }
